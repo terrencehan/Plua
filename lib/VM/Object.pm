1787859ddc130b08c124dfde0de635436cbd42cb
terrencehan <hanliang1990@gmail.com>
2013-02-01 17:37:21 +0800

    add

---
+++ lib/VM/Object.pm
@@ -0,0 +1,13 @@
+use MooseX::Declare;
+
+class Object_ {
+    for (
+        qw/is_nil is_false is_function is_clousre is_string is_number is_table/)
+    {
+        has $_ => (
+            is      => 'rw',
+            isa     => 'Bool',
+            default => 0,
+        );
+    }
+}
