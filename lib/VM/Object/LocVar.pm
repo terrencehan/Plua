54a17b46d69b7bd82d2c618c0f2f9e10fb0c3e42
terrencehan <hanliang1990@gmail.com>
2013-02-02 15:35:27 +0800

    change class name

--- lib/VM/Object/LocVar.pm
+++ lib/VM/Object/LocVar.pm
@@ -1,10 +1,10 @@
-# lib/VM/Object/LocalVar.pm
+# lib/VM/Object/LocVar.pm
 # Copyright (c) 2013 terrencehan
 # hanliang1990@gmail.com
 
 use MooseX::Declare;
 
-class VM::Object::LocalVar {    #no base class
+class VM::Object::LocVar {    #no base class
     has 'var_name' => (
         is  => 'rw',
         isa => 'Str',
