ad991bb66666fc732ceccadba70b11962ef2673f
terrencehan <hanliang1990@gmail.com>
2013-02-16 20:57:13 +0800

    add number.pm and its test file

---
+++ lib/VM/Object/Number.pm
@@ -0,0 +1,81 @@
+# lib/VM/Object/Number.pm
+# Copyright (c) 2013 terrencehan
+# hanliang1990@gmail.com
+
+use MooseX::Declare;
+
+class VM::Object::Number extends VM::Object {
+
+    #-BUILD (value => Num)
+    use lib '../../';
+    use VM::Type;
+
+    has value => (
+        is       => 'rw',
+        isa      => 'Num',
+        required => 1,
+    );
+
+    method BUILD {
+        $self->is_number(1);
+        $self->type( VM::Type->LUA_TNUMBER );
+    }
+
+    override to_string {
+        return "[LuaNumber(" . $self->value . ")]";
+    }
+
+    override to_num { $self->value; }
+
+}
+
+{
+
+    package VM::Object::Number;
+    use overload '==' => \&myeq;
+    use overload '!=' => \&myneq;
+    use overload '+'  => \&add;
+    use overload '-'  => \&sub;
+    use overload '*'  => \&mul;
+    use overload '/'  => \&div;
+
+    sub myeq {
+        my ( $one, $two ) = @_;
+
+        if ( not( ( defined $one ) and ( defined $two ) ) ) {
+            return 0;
+        }
+
+        return $one->value == $two->value;
+    }
+
+    sub myneq {
+        my ( $one, $two ) = @_;
+        return not( $one == $two );
+    }
+
+    sub add {
+        my ( $one, $two ) = @_;
+        VM::Object::Number->new( value => ( $one->value + $two->value ) );
+    }
+
+    sub sub {
+        my ( $one, $two ) = @_;
+        VM::Object::Number->new( value => ( $one->value - $two->value ) );
+
+    }
+
+    sub mul {
+        my ( $one, $two ) = @_;
+        VM::Object::Number->new( value => ( $one->value * $two->value ) );
+
+    }
+
+    sub div {
+        my ( $one, $two ) = @_;
+        VM::Object::Number->new( value => ( $one->value / $two->value ) );
+    }
+    1;
+}
+
+1;
---
+++ t/06-vm-object-number.t
@@ -0,0 +1,35 @@
+# t/06-vm-object-number.t
+# Copyright (c) 2013 terrencehan
+# hanliang1990@gmail.com
+
+use strict;
+use warnings;
+
+use lib '../lib';
+use Test::More tests => 16;
+use VM::Type;
+BEGIN { use_ok('VM::Object::Number') }
+
+my $num1 = VM::Object::Number->new( value => 2 );
+my $num2 = VM::Object::Number->new( value => 3 );
+
+isa_ok $num1, 'VM::Object::Number';
+
+is $num1->to_num, 2;
+
+is $num1->to_string,  "[LuaNumber(2)]";
+is $num1->to_literal, "[LuaNumber(2)]";
+
+is $num1 == $num2, '';
+is $num1 == $num1, 1;
+is $num1 == undef, 0;
+is undef == $num1, 0;
+is $num1 != $num2, 1;
+
+is( ( $num1 + $num2 )->to_num, 5 );
+is( ( $num1 * $num2 )->to_num, 6 );
+is( ( $num1 - $num2 )->to_num, -1 );
+is( ( $num1 / $num2 )->to_num, 2 / 3 );
+
+is $num1->type,      VM::Type->LUA_TNUMBER;
+is $num1->is_number, 1;
