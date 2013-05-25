87ae4c8e96029aa667706278abac90087a46a2a0
terrencehan <hanliang1990@gmail.com>
2013-05-25 13:00:24 +0800

    add comments

--- lib/VM/CallStatus.pm
+++ lib/VM/CallStatus.pm
@@ -14,14 +14,13 @@ class VM::CallStatus {    #enum
 
     my $count = 0;
     for (
-        qw/
-        CIST_LUA
-        CIST_HOOKED
-        CIST_REENTRY
-        CIST_YIELDED
-        CIST_YPCALL
-        CIST_STAT
-        CIST_TAIL/
+        'CIST_LUA',       #call is running a Lua function
+        'CIST_HOOKED',    #call is running a debug hook
+        'CIST_REENTRY',   #call is running on same invocation of luaV_execute of previous call
+        'CIST_YIELDED',   #call reentered after suspension
+        'CIST_YPCALL',    #call is a yieldable protected call
+        'CIST_STAT',      #call has an error status (pcall)
+        'CIST_TAIL',      #call was tail called
       )
     {
         class_has $_ => (
