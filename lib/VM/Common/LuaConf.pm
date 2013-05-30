e7c185c2cdb95a4ce48dcbe4b1b964d93a6996a5
terrencehan <hanliang1990@gmail.com>
2013-05-30 21:24:37 +0800

    add comment

--- lib/VM/Common/LuaConf.pm
+++ lib/VM/Common/LuaConf.pm
@@ -9,11 +9,11 @@ class VM::Common::LuaConf {
 
     my %h;
 
-    $h{LUAI_BITSINT}        = 32;
-    $h{LUAI_MAXSTACK}       = $h{LUAI_BITSINT} >= 32 ? 1000000 : 15000;
-    $h{LUAI_FIRSTPSEUDOIDX} = -$h{LUAI_MAXSTACK} - 1000;
-    $h{LUA_SIGNATURE} = pack "C*", 0x1b, 0x4c, 0x75, 0x61;    #"\u001bLua";
-    $h{LUA_DIRSEP}    = '/';                                  #TODO
+    $h{LUAI_BITSINT} = 32;
+    $h{LUAI_MAXSTACK} = $h{LUAI_BITSINT} >= 32 ? 1000000 : 15000;
+    $h{LUAI_FIRSTPSEUDOIDX} = -$h{LUAI_MAXSTACK} - 1000;           #for registry
+    $h{LUA_SIGNATURE}       = pack "C*", 0x1b, 0x4c, 0x75, 0x61;   #"\u001bLua";
+    $h{LUA_DIRSEP}          = '/';                                 #TODO
 
     for ( keys %h ) {
         class_has $_ => (
