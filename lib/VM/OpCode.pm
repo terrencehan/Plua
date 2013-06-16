d568c52dadb6a9b87de4fb7b438e4cd52c64d5d0
terrencehan <hanliang1990@gmail.com>
2013-06-16 10:00:42 +0800

    fix indent

--- lib/VM/OpCode.pm
+++ lib/VM/OpCode.pm
@@ -38,35 +38,31 @@ BEGIN {
 
         'OP_CONCAT',      #    A B C      R(A) := R(B).. ... ..R(C)
 
-        'OP_JMP', #    A sBx      pc+=sBx; if (A) close all upvalues >= R(A) + 1
-        'OP_EQ',  #    A B C      if ((RK(B) == RK(C)) ~= A) then pc++
-        'OP_LT',  #    A B C      if ((RK(B) <  RK(C)) ~= A) then pc++
-        'OP_LE',  #    A B C      if ((RK(B) <= RK(C)) ~= A) then pc++
+        'OP_JMP',         #    A sBx      pc+=sBx; if (A) close all upvalues >= R(A) + 1
+        'OP_EQ',          #    A B C      if ((RK(B) == RK(C)) ~= A) then pc++
+        'OP_LT',          #    A B C      if ((RK(B) <  RK(C)) ~= A) then pc++
+        'OP_LE',          #    A B C      if ((RK(B) <= RK(C)) ~= A) then pc++
 
-        'OP_TEST',    #    A C        if not (R(A) <=> C) then pc++
-        'OP_TESTSET'
-        ,    #    A B C      if (R(B) <=> C) then R(A) := R(B) else pc++
+        'OP_TEST',        #    A C        if not (R(A) <=> C) then pc++
+        'OP_TESTSET',     #    A B C      if (R(B) <=> C) then R(A) := R(B) else pc++
 
-        'OP_CALL'
-        ,    #    A B C      R(A), ... ,R(A+C-2) := R(A)(R(A+1), ... ,R(A+B-1))
-        'OP_TAILCALL',  #    A B C      return R(A)(R(A+1), ... ,R(A+B-1))
-        'OP_RETURN',    #    A B        return R(A), ... ,R(A+B-2)    (see note)
+        'OP_CALL',        #    A B C      R(A), ... ,R(A+C-2) := R(A)(R(A+1), ... ,R(A+B-1))
+        'OP_TAILCALL',    #    A B C      return R(A)(R(A+1), ... ,R(A+B-1))
+        'OP_RETURN',      #    A B        return R(A), ... ,R(A+B-2)    (see note)
 
-        'OP_FORLOOP',   #    A sBx      R(A)+=R(A+2);
-            #               if R(A) <?= R(A+1) then { pc+=sBx; R(A+3)=R(A) }
-        'OP_FORPREP',    #    A sBx      R(A)-=R(A+2); pc+=sBx
+        'OP_FORLOOP',     #    A sBx      R(A)+=R(A+2);
+                          #               if R(A) <?= R(A+1) then { pc+=sBx; R(A+3)=R(A) }
+        'OP_FORPREP',     #    A sBx      R(A)-=R(A+2); pc+=sBx
 
-        'OP_TFORCALL'
-        ,    #    A C        R(A+3), ... ,R(A+2+C) := R(A)(R(A+1), R(A+2));
-        'OP_TFORLOOP'
-        ,    #    A sBx      if R(A+1) ~= nil then { R(A)=R(A+1); pc += sBx }
+        'OP_TFORCALL',    #    A C        R(A+3), ... ,R(A+2+C) := R(A)(R(A+1), R(A+2));
+        'OP_TFORLOOP',    #    A sBx      if R(A+1) ~= nil then { R(A)=R(A+1); pc += sBx }
 
-        'OP_SETLIST',   #    A B C      R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
+        'OP_SETLIST',     #    A B C      R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
 
-        'OP_CLOSURE',   #    A Bx       R(A) := closure(KPROTO[Bx])
+        'OP_CLOSURE',     #    A Bx       R(A) := closure(KPROTO[Bx])
 
-        'OP_VARARG',    #    A B        R(A), R(A+1), ..., R(A+B-2) = vararg
-        'OP_EXTRAARG'   #Ax	extra (larger) argument for previous opcode	*/
+        'OP_VARARG',      #    A B        R(A), R(A+1), ..., R(A+B-2) = vararg
+        'OP_EXTRAARG'     #    Ax         extra (larger) argument for previous opcode
 
     );
 
